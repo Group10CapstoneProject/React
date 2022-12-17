805103991cd22e09b689454f60ed6d0d63255f2e
Muhammad Rizky Ferdiansyah <muhammadrizkyferdiansyah@gmail.com>
2022-12-18 02:39:47 +0700

    updated new footer

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -515,33 +515,69 @@ function LandingPage() {
         </div>
       </div>
 
-      <footer className="footer p-10 bg-info text-white pt-[100px]">
-        <div className="ml-[100px] mb-[100px]">
-          <img src={logofooterImage} alt="logo footer" />
-          <p>
-            altagym@gmail.com
-            <br />
-            (024) 20192933
-          </p>
-        </div>
-        <div className="px-[10]">
-          <span className="footer-title text-2xl normal-case">Bantuan</span>
-          <a className="link link-hover">FAQ</a>
-          <a className="link link-hover">Syarat & Ketentuan</a>
-          <a className="link link-hover">Kebijakan Privasi</a>
-        </div>
-        <div>
-          <span className="footer-title normal-case">Dapat Aplikasinya</span>
-          <img className="w-40" src={playImage} alt="playstore" />
-        </div>
-
-        {/* <div className="pt-[100px]">
-          <box-icon name="facebook-square" type="logo" color="#3b5998"></box-icon>
-          <box-icon name="instagram" type="logo" color="#cd486b"></box-icon>
-          <box-icon name="twitter" type="logo" color="#00acee"></box-icon>
-          <box-icon name="youtube" type="logo" color="#f50404"></box-icon>
-        </div> */}
-      </footer>
+      <div>
+        <footer className="footer p-10 bg-info text-base flex justify-evenly">
+          <div className="">
+            <img src={logofooterImage} alt="logo footer" />
+            <p>
+              <box-icon name="envelope" color="#ffffff"></box-icon>
+              {"  "}
+              altagym@gmail.com
+              <br />
+              <box-icon name="phone" color="#ffffff"></box-icon>
+              {"  "}
+              (024) 20192933
+            </p>
+          </div>
+          <div>
+            <span className="footer-title normal-case">Dapat Aplikasinya</span>
+            {/* <img className="w-40" src={playImage} alt="playstore" /> */}
+            <a
+              href="#"
+              class="w-full sm:w-auto bg-black hover:bg-black focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-black dark:hover:bg-gray-800 dark:focus:ring-gray-800"
+            >
+              <svg class="mr-3 w-7 h-7" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="google-play" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                <path
+                  fill="currentColor"
+                  d="M325.3 234.3L104.6 13l280.8 161.2-60.1 60.1zM47 0C34 6.8 25.3 19.2 25.3 35.3v441.3c0 16.1 8.7 28.5 21.7 35.3l256.6-256L47 0zm425.2 225.6l-58.9-34.1-65.7 64.5 65.7 64.5 60.1-34.1c18-14.3 18-46.5-1.2-60.8zM104.6 499l280.8-161.2-60.1-60.1L104.6 499z"
+                ></path>
+              </svg>
+              <div class="text-left">
+                <div class="mb-1 text-xs">GET IT ON</div>
+                <div class="-mt-1 font-sans text-sm font-semibold">Google Play</div>
+              </div>
+            </a>
+          </div>
+          <div>
+            <span className="footer-title text-2xl normal-case">Bantuan</span>
+            <a className="link link-hover">FAQ</a>
+            <a className="link link-hover">Syarat & Ketentuan</a>
+            <a className="link link-hover">Kebijakan Privasi</a>
+          </div>
+          <div>
+            <div className="grid grid-flow-col gap-4">
+              <a>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current">
+                  <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
+                </svg>
+              </a>
+              <a>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current">
+                  <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
+                </svg>
+              </a>
+              <a>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current">
+                  <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
+                </svg>
+              </a>
+            </div>
+          </div>
+          <div>
+            <p>© Copyright 2022 .AltaGym. All Right Reserved.</p>
+          </div>
+        </footer>
+      </div>
     </div>
   );
 }
